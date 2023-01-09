cd9fe7a2be643cfbca915dac586924ef94a10b1d
sh1lpa <shilpashinde25@gmail.com>
2023-01-09 14:26:10 +0530

    Example of print object in python

--- print_class_object__str__.py
+++ print_class_object__str__.py
@@ -1,50 +1,22 @@
 class Person:
 
-    def __init__(self, name, occupation, salary) -> None:
+    def __init__(self, name, age, salary) -> None:
         self.name= name
-        self.occupation = occupation
+        self.age = age
         self.salary = salary
 
 
     def __str__(self) -> str:
-        return f'Name : {self.name}\tOccupation : {self.occupation}\tSalary : {self.salary}'
+        return f'Name : {self.name}\tage : {self.age}\tSalary : {self.salary}'
 
     def __repr__(self) -> str:
-        return f'Name : {self.name}\tOccupation : {self.occupation}\tSalary : {self.salary}'
-
-
-class Teacher(Person):
-
-    def __init__(self, name, occupation, subject, salary) -> None:
-        super().__init__(name, occupation, salary)
-        self.subject = subject
-
-    def __str__(self) -> str:
-        return f'Name : {self.name}\tOccupation : {self.occupation}\tSubject : {self.subject}\tSalary : {self.salary}'
-
-    def __repr__(self) -> str:
-        return f'Name : {self.name}\tOccupation : {self.occupation}\tSubject : {self.subject}\tSalary : {self.salary}'
-
-class Doctor(Person):
-
-    def __init__(self, name, occupation, speciality, salary) -> None:
-        super().__init__(name, occupation, salary)
-        self.speciality = speciality
-
-    def __str__(self) -> str:
-        return f'Name : {self.name}\tOccupation : {self.occupation}\speciality : {self.speciality}\tSalary : {self.salary}'
-
-    def __repr__(self) -> str:
-        return f'Name : {self.name}\tOccupation : {self.occupation}\tspeciality : {self.speciality}\tSalary : {self.salary}'
+        return f'Name : {self.name}\tage : {self.age}\tSalary : {self.salary}'
 
 
 
 def main():
-    Rahul = Teacher('Rahul', 'Teacher','Maths','50000')
+    Rahul = Person('Rahul', '25','50000')
     print(Rahul)
-
-    Riya = Doctor('Riya', 'Doctor', 'Dentist', '55000')
-    print(Riya)
 
 
 if __name__=='__main__':
